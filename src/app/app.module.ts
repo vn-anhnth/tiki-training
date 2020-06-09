71dbabe4845e33c5bdf20a2a60f4b6d04240a710
vn-anhnth <anhnth@rakumo.vn>
2020-06-09 17:27:23 +0700

    create a page to show products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MainComponent } from './components/main/main.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ProductListComponent } from './components/main/product-list/product-list.component';
+import { ProductDetailComponent } from './components/main/product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MainComponent,
-    FooterComponent
+    FooterComponent,
+    ProductListComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
